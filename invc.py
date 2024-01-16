266e144d4091f917c7b0e8c2d87c998f3e5cc7dc
Abhinav Yukth S <93666264+Abhinavyukth-Suresh@users.noreply.github.com>
2024-01-17 00:46:50 +0530

    Add files via upload

---
+++ invc.py
@@ -0,0 +1,24 @@
+import numpy as np 
+import ctypes ,os
+import time
+n=2000
+A  = np.random.random((n,n))
+
+c_float_p = ctypes.POINTER(ctypes.c_double)
+data = A.flatten()
+data = data.astype(np.float64)
+data_p = data.ctypes.data_as(c_float_p)
+path = os.getcwd()
+
+cdll = ctypes.CDLL(path+'/./test.so',winmode=0)
+cdll.MATRIX_INV.restype = ctypes.POINTER(ctypes.c_double * (n*n))
+t = time.time()
+DATA = cdll.MATRIX_INV(data_p,ctypes.c_int(n))
+t2= time.time()
+
+k = [i for i in DATA.contents ]
+
+#print(k)
+b = np.array(k).reshape(n,n)
+#print(b.dot(A).round(2))
+print(t2-t)
